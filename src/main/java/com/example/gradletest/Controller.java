b57f2d511455e54f8be7e85863c1cfa736392b28
ori.livne <ori.livne01@gmail.com>
2023-12-31 17:01:45 +0200

    First commit

---
+++ src/main/java/com/example/gradletest/Controller.java
@@ -0,0 +1,13 @@
+package com.example.gradletest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/gradle")
+public class Controller {
+    @RequestMapping("/test")
+    public String test(){
+        return "test";
+    }
+}
